78e97ae9aed2023ed8bcbeb971c1c6d75bc50faf
Michael Krog <michaelryankrog@gmail.com>
2019-05-05 13:55:32 -0600

    updated readme for endpoints

--- db/data/seedData.js
+++ db/data/seedData.js
@@ -77,7 +77,312 @@ let concertData = [
       }
     ]
   },
-
+  {
+    name: 'Papadosio & The Polish Ambassador',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'SATURDAY May 11, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'http://www.axs.com/events/366910/papadosio-the-polish-ambassador-ft-wildlight-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Luke Combs',
+    genre: 'Country',
+    concerts: [
+      {
+        date: 'SUNDAY May 12, 2019',
+        time_start: '7:00 PM',
+        time_doors: '5:30 PM',
+        tickets_link: 'http://www.axs.com/events/361018/luke-combs-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Lord Huron',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'TUESDAY May 14, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/363296/lord-huron-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Walk Off The Earth',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'WEDNESDAY May 15, 2019',
+        time_start: '6:30 PM',
+        time_doors: '5:30 PM',
+        tickets_link: 'https://www.axs.com/events/366566/walk-off-the-earth-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Florence + The Machine',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'MONDAY May 20, 2019',
+        time_start: '7:30 PM',
+        time_doors: '6:30 PM',
+        tickets_link: 'https://www.axs.com/events/369043/florence-the-machine-the-high-as-hope-tour-2019-tickets?skin=redrocks',
+      },
+      {
+        date: 'TUESDAY May 21, 2019',
+        time_start: '7:30 PM',
+        time_doors: '6:30 PM',
+        tickets_link: 'https://www.axs.com/events/369157/florence-the-machine-the-high-as-hope-tour-2019-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'The Devil Makes Three',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'FRIDAY May 24, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/364687/the-devil-makes-three-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'The Disco Biscuits',
+    genre: 'Jam',
+    concerts: [
+      {
+        date: 'SATURDAY May 24, 2019',
+        time_start: '6:30 PM',
+        time_doors: '5:30 PM',
+        tickets_link: 'https://www.axs.com/events/368375/the-disco-biscuits-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Chromeo / Thievery Corporation',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'FRIDAY May 31, 2019',
+        time_start: '6:00 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/367755/chromeo-thievery-corporation-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Moe.',
+    genre: 'Jam',
+    concerts: [
+      {
+        date: 'SATURDAY June 01, 2019',
+        time_start: '6:00 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/365396/moe-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Parade of Bass FT. Dillion Francis',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'SUNDAY June 01, 2019',
+        time_start: '5:00 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/371057/parade-of-bass-ft-dillon-francis-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Parade of Bass FT. Dillion Francis',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'SUNDAY June 01, 2019',
+        time_start: '5:00 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/371057/parade-of-bass-ft-dillon-francis-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Billie Eilish',
+    genre: 'Country',
+    concerts: [
+      {
+        date: 'WEDNESDAY June 05, 2019',
+        time_start: '7:30 PM',
+        time_doors: '6:30 PM',
+        tickets_link: 'https://www.axs.com/events/369165/billie-eilish-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Brit Floyd',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'THURSDAY June 06, 2019',
+        time_start: '8:00 PM',
+        time_doors: '6:30 PM',
+        tickets_link: 'https://www.axs.com/events/367469/brit-floyd-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Michael Franti and Spearhead Floyd',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'FRIDAY June 07, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:30 PM',
+        tickets_link: 'https://www.axs.com/events/363373/michael-franti-spearhead-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Big Head Todd and The Monsters',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'SATURDAY June 08, 2019',
+        time_start: '6:30 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/366026/big-head-todd-and-the-monsters-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Dispatch Summer Stops 2019',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'SUNDAY June 09, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/368406/dispatch-summer-stops-2019-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Sublime with Rome',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'MONDAY June 10, 2019',
+        time_start: '6:00 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/369080/sublime-with-rome-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Rainbox Kitten Suprise',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'TUESDAY June 11, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/366569/rainbow-kitten-surprise-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Young The Giant / Fitz and The Tantrums',
+    genre: 'Rock',
+    concerts: [
+      {
+        date: 'WEDNESDAY June 12, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/369876/young-the-giant-fitz-the-tantrums-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'San Holo',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'THURSDAY June 13, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/368663/san-holo-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Lettuce',
+    genre: 'Jam',
+    concerts: [
+      {
+        date: 'SATURDAY June 15, 2019',
+        time_start: '6:00 PM',
+        time_doors: '5:30 PM',
+        tickets_link: 'https://www.axs.com/events/366344/lettuce-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Turnpike Troubadours',
+    genre: 'Country',
+    concerts: [
+      {
+        date: 'TUESDAY June 18, 2019',
+        time_start: '6:30 PM',
+        time_doors: '5:00 PM',
+        tickets_link: 'https://www.axs.com/events/370441/turnpike-troubadours-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Zedd On The Rocks',
+    genre: 'Electronic',
+    concerts: [
+      {
+        date: 'WEDNESDAY June 19, 2019',
+        time_start: '6:00 PM',
+        time_doors: '5:30 PM',
+        tickets_link: 'https://www.axs.com/events/369860/zedd-on-the-rocks-tickets?skin=redrocks',
+      }
+    ]
+  },
+  {
+    name: 'Umphreys Mcgee',
+    genre: 'Jam',
+    concerts: [
+      {
+        date: 'FRIDAY June 12, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/365938/umphrey-s-mcgee-tickets?skin=redrocks',
+      },
+      {
+        date: 'SATURDAY June 22, 2019',
+        time_start: '7:00 PM',
+        time_doors: '6:00 PM',
+        tickets_link: 'https://www.axs.com/events/365939/umphrey-s-mcgee-tickets?skin=redrocks',
+      },
+      {
+        date: 'SUNDAY June 23, 2019',
+        time_start: '5:00 PM',
+        time_doors: '4:00 PM',
+        tickets_link: 'https://www.axs.com/events/365940/umphrey-s-mcgee-tickets?skin=redrocks',
+      }
+    ]
+  }
 ]
 
 module.exports = concertData;
--- server.js
+++ server.js
@@ -4,48 +4,21 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 const app = express()
-// const port = 3000
 const PORT = process.env.PORT || 3000;
-// app.set('port', process.env.PORT || 3000)
 
 app.use(express.json())
-
-// {
-//   name: 'The 1975',
-//   genre: 'Rock',
-//   concerts: [
-//     {
-//       date: 'TUESDAY April 30, 2019',
-//       time_start: '7:00 PM',
-//       time_doors: '6:00 PM',
-//       tickets_link: 'https://www.axs.com/events/363535/the-1975-tickets?skin=redrocks',
-//     }
-//   ]
-// },
-
-// {
-// 	"name": "New Band",
-// 	"genre": "Classical"
-// }
-
-//
-// {
-//   "concerts": [
-//     {
-//       "date": "SAT Test",
-//       "time_start": "7 PM",
-//       "time_doors": "60 PM",
-//       "tickets_link": "redrocks"
-//     }
-//   ]
-// }
-
 
 // GET All Concerts
 app.get('/api/v1/concerts', (request, response) => {
   database('concerts').select()
     .then((concerts) => {
-      response.status(200).json(concerts);
+      if(concerts.length) {
+        response.status(200).json(concerts);
+      } else {
+        response.status(404).json({
+          error: `Could not find concerts`
+        });
+      }
     })
     .catch((error) => {
       response.status(500).json({ error });
@@ -56,7 +29,13 @@ app.get('/api/v1/concerts', (request, response) => {
 app.get('/api/v1/bands', (request, response) => {
   database('bands').select()
     .then((bands) => {
-      response.status(200).json(bands);
+      if(bands.length) {
+        response.status(200).json(bands);
+      } else {
+        response.status(404).json({
+          error: `Could not find bands`
+        });
+      }
     })
     .catch((error) => {
       response.status(500).json({ error });
@@ -119,27 +98,32 @@ app.post('/api/v1/bands', (request, response) => {
   });
 })
 
-// DELETE A Specific Band
-app.delete('/api/v1/bands/:id', (request, response) => {
+// DELETE A Specific Band and associated concerts
+app.delete('/api/v1/bands/:id/concerts', (request, response) => {
 
   database('concerts').where({ concertId: request.params.id }).del()
     .then(concertID => {
-      console.log(concertID);
+      if(concertID > 0) {
+        database('bands').where({ id: request.params.id }).del()
+        .then(id => {
+          if(id > 0) {
+            response.status(202).json('Band and Concerts Deleted')
+          } else {
+            response.status(404).json({
+              error: `Could not find or delete band with id ${request.params.id}`
+            });
+          }
+        })
+      } else {
+        response.status(404).json({
+          error: `Could not find or delete concert with concerID ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
     })
 
-
-  // database('bands').where({ id: request.params.id }).del()
-  //   .then(band => {
-  //     if (band > 0) {
-  //       response.status(202).json('Band Deleted');
-  //     } else {
-  //       response.status(404).json({
-  //         error: `Could not find or delete band with id ${request.params.id}`
-  //       });
-  //     }
-  //   }).catch(error => {
-  //     response.status(500).json({ error });
-  //   })
 })
 
 // POST A Specific Concert
@@ -192,101 +176,6 @@ app.delete('/api/v1/concerts/:id', (request, response) => {
 })
 
 
-
-
-app.put('/api/v1/bands/:id', (request, response) => {
-  let { id } = request.params;
-
-  database('bands').where('id', id)
-    .update({
-      name: 'Homer'
-    })
-    .then(concert => {
-      response.status(201).json('Successfully Posted Bands')
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-
-});
-
-
-
-
-// PUT Update Content
-app.put('/api/v1/concerts/:id', (request, response) => {
-  let { id } = request.params;
-  let { date, time_start, time_doors, tickets_link, bands } = request.body;
-
-  database('concerts').where('id', id).select()
-    .then(concert => {
-      if(concert.length){
-        database('concerts').where('id', id).update({
-          date: request.body.date || concert[0].date,
-          time_start: request.body.time_start || concert[0].time_start,
-          time_doors: request.body.time_doors || concert[0].time_doors,
-          tickets_link: request.body.tickets_link || concert[0].tickets_link,
-        }).then(concert => {
-          if(request.body.bands){
-            database('bands').where('concertId', request.params.id).select()
-              .then(bandsDB => {
-                Promise.all(bands.map((band, i) => {
-                  console.log(band);
-                  return database('bands').where('id', bandsDB[i].id)
-                    .update({
-                      name: request.body.bands[i].name || bandsDB[i].name,
-                      headliner: request.body.bands[i].headliner || bandsDB[i].headliner
-                    })
-                })).then(more => {
-                  response.status(201).json('Successfully Updated Concerts and Bands')
-                })
-              })
-          } else {
-            response.status(201).json('Successfully Updated Concerts')
-          }
-        })
-        .catch(error => {
-          response.status(500).json({ error });
-        });
-      }
-    })
-
-})
-
-
-// for(let i = 0; i < band.length; i++) {
-//   console.log(band[i].name);
-//   console.log(request.body.bands[i].name);
-//   database('bands').where('id', band[i].id)
-//     .update({
-//       name: request.body.bands[i].name || band[i].name,
-//       headliner: request.body.bands[i].headliner || band[i].headliner
-//     })
-// }
-//
-// {
-//   "date": "THURSDAY May 09, 2019",
-//   "time_start": "7:00 PM",
-//   "time_doors": "6:00 PM",
-//   "tickets_link": "https://www.axs.com/events/364883/vulfpeck-tickets?skin=redrocks",
-//   "bands": [
-//     { "name": "Vulfpeck", "headliner": true },
-//     { "name": "with Khruangbin, Cory Henry", "headliner": false }
-//   ]
-// }
-
-// knex('users')
-//   .where({ id: 2 })
-//   .update({ name: 'Homer' })
-// knex('books')
-//   .where('published_date', '<', 2000)
-//   .update({
-//     status: 'archived',
-//     thisKeyIsSkipped: undefined
-//   })
-
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
